48684b7816634134fba3efb63923db3e48c7a716
itS-ShhB <its.shhwb@gmail.com>
2023-06-18 19:53:54 +0430

    1

--- components/module/Card.js
+++ components/module/Card.js
@@ -1,6 +1,7 @@
 import styles from "./Card.module.css";
 import Location from "../icons/Location";
 import Link from "next/link";
+import Image from "next/image";
 
 function Card(props) {
   const { id, name, model, year, distance, location, image, price } = props;
@@ -8,7 +9,7 @@ function Card(props) {
   return (
     <Link href={`/cars/${id}`}>
       <div className={styles.container}>
-        <img src={image} className={styles.image} />
+        <Image src={image} className={styles.image} alt="Car Image" />
         <h4 className={styles.title}> {`${name} ${model}`} </h4>
         <p className={styles.detail}>{`${year} . ${distance}KM `}</p>
         <div className={styles.footer}>
